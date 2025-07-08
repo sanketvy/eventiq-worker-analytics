eaf01c9ac1b74bd115332356c10eac758631dffb
Sanket Vyawahare <srvyawahare18@gmail.com>
2025-07-08 22:12:00 +0530

    Initial project setup for worker process

---
+++ src/main/java/com/eventiq/worker/analytics/EventiqAnalyticsWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.eventiq.worker.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventiqAnalyticsWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventiqAnalyticsWorkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eventiq/worker/analytics/EventiqAnalyticsWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventiq.worker.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventiqAnalyticsWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
